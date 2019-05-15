06bf411e36e67593fee2cff3242ef90b67101d21
dmytro.kuts <dmytro.kuts@unity-bars.com>
2019-05-15 14:09:50 +0300

    Update Page Main Rozetka

--- src/main/java/Actions/MainActions.java
+++ src/main/java/Actions/MainActions.java
@@ -1,24 +1,35 @@
 package Actions;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import org.openqa.selenium.JavascriptExecutor;
+
+import java.io.*;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class MainActions {
-    public void writerFile(SelenideElement nameFile) {
+    public void writerFile( String numberPage,ElementsCollection nameModel, String nameFile) {
+        //WebDriverWait wait = new WebDriverWait(getWebDriver(), 20);
+
         try {
-            FileWriter writer = new FileWriter("nameModels.txt", true);
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(nameFile, true));
+            writer.write(numberPage + ": " +nameModel.texts() + "\n");
             writer.flush();
-            writer.write(nameFile.getText() + "\n");
             writer.close();
         } catch (
                 IOException ex) {
+
         }
     }
-    public SelenideElement returnUpdXpath(String st1, String st2, String st3){
+    public SelenideElement returnUpdXpath(String st1, Integer st2, String st3){
         return $x(st1+st2+st3);
+    }
+
+    public void scrollToElement(SelenideElement element) {
+        ((JavascriptExecutor) getWebDriver()).executeScript("arguments[0].scrollIntoView();", element);
     }
 }
--- src/main/java/pageObjects/MainRozetkaCom.java
+++ src/main/java/pageObjects/MainRozetkaCom.java
@@ -3,26 +3,39 @@ package pageObjects;
 import Actions.MainActions;
 import com.codeborne.selenide.Condition;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class MainRozetkaCom {
     private SelenideElement telephoneAndElectronics =  $x("//a[@class='menu-categories__link' and text() = 'Смартфоны, ТВ и электроника']");
     private SelenideElement telephones =  $x("//a[@class='m-cat-l-i-title-link' and text() = 'Телефоны']");
     private SelenideElement smartphones =  $x("//a[@class='m-cat-l-i-title-link' and text() = 'Смартфоны']");
-    //private SelenideElement nameModel = $x(String.format("(//div[@class='g-i-tile-i-title clearfix'])[%s]"));
+    private ElementsCollection nameModels = $$x("//div[@class='g-i-tile-i-title clearfix']");
+    private SelenideElement page2 =  $(By.id("page2"));
+    private SelenideElement page3 =  $(By.id("page3"));
+    private SelenideElement logo = $x("//div[@class='logo']");
+    private ElementsCollection nameModels2;
 
-
-    public MainRozetkaCom openSmartphonesMenu(){
+    public  MainRozetkaCom openSmartphonesMenu(){
         MainActions mainActions = new MainActions();
-        SelenideElement nameModel = mainActions.returnUpdXpath("(//div[@class='g-i-tile-i-title clearfix'])[", "1","]");
 
-        telephoneAndElectronics.shouldBe(Condition.visible).click();
-        telephones.shouldBe(Condition.visible).click();
-        smartphones.shouldBe(Condition.visible).click();
-        nameModel.shouldBe(Condition.visible);
-        mainActions.writerFile(nameModel);
+        telephoneAndElectronics.shouldBe(Condition.enabled).click();
+        telephones.shouldBe(Condition.enabled).click();
+        smartphones.shouldBe(Condition.enabled).click();
+        mainActions.writerFile(  "Page1 ", nameModels, "nameModels.txt");
+        mainActions.scrollToElement(page2);
+        nameModels2.addAll(nameModels);
+        page2.click();
+        logo.waitUntil(Condition.visible, 4000);
+        mainActions.writerFile("Page2 ",nameModels, "nameModels.txt");
+        mainActions.scrollToElement(page3);
+        page3.click();
+        logo.waitUntil(Condition.enabled, 4000);
+        mainActions.writerFile("Page3 ",nameModels, "nameModels.txt");
 
         return new MainRozetkaCom();
     }
